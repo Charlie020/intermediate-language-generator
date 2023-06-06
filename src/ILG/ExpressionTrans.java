0ddc5401bc4b55288883e5e2cff5a4e23ffcb239
25803 <2580390720@qq.com>
2023-06-06 19:34:58 +0800

    能够利用逆波兰式产生三元式和四元式

--- src/ILG/ExpressionTrans.java
+++ src/ILG/ExpressionTrans.java
@@ -57,7 +57,7 @@ public class ExpressionTrans {
                 Operator.push(Character.toString(ch));
             } else if (ch == ')') {
                 // 右括号则退栈至左括号为止
-                if (ans.charAt(ans.length() - 1) == ' ') ans = ans.substring(0, ans.length() - 1);
+                if (ans.charAt(ans.length() - 1) == ' ') ans = ans.substring(0, ans.length() - 1); // 处理格式，两个参数间至多一个空格，方便处理
                 while (!Operator.isEmpty() && !Operator.peek().equals("(")) ans += " " + Operator.pop();
                 if (!Operator.isEmpty() && Operator.peek().equals("(")) Operator.pop();
             } else if (isOperator(ch)) {
@@ -81,17 +81,71 @@ public class ExpressionTrans {
         }
         // 清空运算符栈
         while (Operator.size() != 0) ans += " " + Operator.pop();
+        ans += "\n";
         return ans;
     }
 
-    // 三元式
-    public void ThreeAddressCode(String text) {
+    // 求三元式和四元式
+    public String[] ThreeAddressCode_Quadruple(String IP) {
+        Stack<String> TACArgs = new Stack<>();  // 三元式参数栈
+        Stack<String> QDPArgs = new Stack<>();  // 四元式参数栈
+        String TAC = new String("");
+        String QDP = new String("");
 
-    }
+        String args = new String("");
+        String op = new String("");
+        IP += " "; // 由于逆波兰式中各参数间以空格相隔，此处方便得到最后一个三元式
+        int i = 0, len = IP.length(), count = 0;
+        while (i < len) {
+            char ch = IP.charAt(i);
+            if (isDigit(ch) || isLetter(ch)) {      // 遇参数直接入栈
+                while (isDigit(ch) || isLetter(ch)) {
+                    args += ch;
+                    ++i;
+                    ch = IP.charAt(i);
+                }
+                TACArgs.push(args);
+                QDPArgs.push(args);
+                args = "";
+            } else if (isOperator(ch)) {            // 遇运算符则弹出参数
+                while (isOperator(ch)) {
+                    op += ch;
+                    ++i;
+                    ch = IP.charAt(i);
+                }
+                // 产生三元式
+                ++count;
+                if (op.equals("++") || op.equals("--") || op.equals("!") || op.equals("~")) {   // 单目运算符
+                    // 三元组
+                    String TACFirst = TACArgs.peek();
+                    TAC += "(" + count + ") " + "(" + op + ", " + TACFirst + ", _ " + ")\n";
 
-    // 四元式
-    public void Quadruple(String text) {
+                    // 四元组
+                    String QDPFirst = QDPArgs.peek();
+                    QDP += "(" + count + ") " + "(" + op + ", " + QDPFirst + ", _ " + ", T" + count + ")\n";
+                } else {        // 双目运算符
+                    // 三元组
+                    String TACSecond = TACArgs.pop();      // 先出栈为第二个运算对象
+                    String TACFirst = TACArgs.pop();       // 后出栈为第一个运算对象
+                    TAC += "(" + count + ") " + "(" + op + ", " + TACFirst + ", " + TACSecond + ")\n";
 
+                    // 四元组
+                    String QDPSecond = QDPArgs.pop();
+                    String QDPFirst = QDPArgs.pop();
+                    QDP += "(" + count + ") " + "(" + op + ", " + QDPFirst + ", " + QDPSecond + ", T" + count + ")\n";
+                }
+
+                TACArgs.push("(" + count + ")");
+                QDPArgs.push("T" + count);
+                op = "";
+            }
+            ++i;
+        }
+
+        String[] ans = new String[2];
+        ans[0] = TAC;
+        ans[1] = QDP;
+        return ans;
     }
 
     private boolean Check(String text) {
--- src/ILG/Window.java
+++ src/ILG/Window.java
@@ -5,32 +5,41 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Window extends JFrame {
-    public JButton confirm = new JButton("确定");
-    public JTextField input = new JTextField("Input here", 20);
+    private JButton confirm = new JButton("确定");
+    private JTextField input = new JTextField("Input here", 20);
+    private JTextArea outputArea = new JTextArea();
 
     public Window() {
         // 设置组件布局
-        setLayout(new FlowLayout());
+        this.setLayout(new GridBagLayout());
         input.setPreferredSize(new Dimension(200, 30));
         input.setFont(new Font("Arial", Font.PLAIN,16));
+        input.setForeground(Color.GRAY);
         confirm.setSize(200, 60);
+        outputArea.setEditable(false);
 
         // 添加组件
         this.add(input);
         this.add(confirm);
+        this.add(outputArea);
 
         // 监听器
         confirm.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String text = input.getText();
-                ClearSpaces(text);
+                if (input.getText().equals("Input here")) {
+                    JOptionPane.showMessageDialog(Window.this, "请输入内容！");
+                } else {
+                    String text = input.getText();
+                    text = ClearSpaces(text);
 
-                ExpressionTrans ET = new ExpressionTrans();
-                String IP = ET.InversePolish(text);
-                System.out.println(IP);
-                ET.ThreeAddressCode(IP);
-                ET.Quadruple(IP);
+                    ExpressionTrans ET = new ExpressionTrans();
+                    String IP = ET.InversePolish(text);                // 产生逆波兰式
+                    String ans[] = ET.ThreeAddressCode_Quadruple(IP);  // 利用逆波兰式产生三元式和四元式
+                    String TAC = ans[0];
+                    String QDP = ans[1];
+                    outputArea.setText("逆波兰式：\n" + IP + "\n三元式：\n" + TAC + "\n四元式：\n" + QDP);
+                }
             }
         });
 
